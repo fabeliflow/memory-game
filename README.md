c80471530dbb3b85acfa39fdeb22a116749d9bda
fabeliflow <fabeliflow@gmail.com>
2018-01-08 07:28:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Udacity Front End Nanodegree Memory Game
+
+##To start:
+
+Double click on index.html to start.
+
+##How to play:
+
+1. Click on any card. Once clicked, the card will flip over and reveal its figure.
+2. Click on another card. If this card's figure matches with the previously flipped one, the cards will turn green and you will have a match!
+3. If the flipped cards do not match, they will flip back facing down
+4. You win the game once you match ALL cards!
+5. The game keeps track of your score and time. Your score will change depending on how many moves it takes you to win the game. One more is equal to 2 flipped cards.
+6. HAVE FUN!
---
+++ js/custom.js
@@ -0,0 +1,185 @@
+/*
+* Custom.js This file contains all the necessary logic to run the memory game
+*/
+
+$(document).ready(function(){
+
+  // This variable contains all required methods and variables for the memory game
+  var memoryGame = {
+
+    // Card figures
+    figures: ['bicycle', 'leaf', 'cube', 'anchor', 'bolt', 'diamond', 'bomb', 'empire'],
+
+    moves: 0,
+
+    timer: null,
+
+    // Start the memory game
+    startGame: function() {
+
+      // Setup timer for
+      timer = new Timer();
+
+      timer.start();
+
+      timer.addEventListener('secondsUpdated', function (e) {
+        $('.game-timer').html(timer.getTimeValues().toString());
+      });
+
+      // Append other set of figures
+      this.figures = this.figures.concat(this.figures);
+
+      // Generate memory game cards
+      this.generateCards();
+    },
+
+    // Shuffle figures (*Updated version from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+    shuffle: function() {
+
+      // Loop through array backwards
+      for (var i = this.figures.length - 1; i > 0; i--) {
+
+        // Random number between i and 1
+        var rand = Math.floor(Math.random() * (i + 1));
+
+        // Make sure it's not the same card (*updated with if statement to remove redundancy if same index)
+        if (i != rand) {
+          var temp = this.figures[i];
+          this.figures[i] = this.figures[rand];
+          this.figures[rand] = temp;
+        }
+      }
+    },
+
+    // Assign figures to card elements within the Memory table
+    generateCards: function() {
+
+      // Empty table
+      $('.m-table').empty();
+
+      // Shuffle figures
+      this.shuffle();
+
+      // Append cards to table
+      for (var i = 0; i < this.figures.length; i++) {
+        $('.m-table').append($('<li id=" card-' + i + '" class="card wow"></li>'));
+      }
+
+      this.flip(this.figures);
+    },
+
+    // Flips a card
+    flip: function(figures) {
+
+      $('.card').click(function() {
+
+        // Check if player selected a maximum of 2 cards and they are not already matched
+        if (!$(this).hasClass('matched') && $('.selected').length < 2) {
+
+          $(this).addClass('selected');
+
+          // Retrieve card id index
+          var cardId = $(this).attr('id').substr(6);
+
+          // Retrieve figure class
+          var figureClass = 'fa-' + figures[cardId];
+
+          // Retrieve icon tag and replace default class with figure class
+          $(this).append('<i class="fa ' + figureClass + '"></i>');
+
+          memoryGame.isAMatch();
+        }
+      });
+    },
+
+    // Check if we have a Match
+    isAMatch: function() {
+
+      setTimeout(function() {
+
+        // If 2 cards have been selected
+        if ($('.selected').length === 2) {
+
+          // Update score panel
+          $('.score-panel').find('.moves').html(++memoryGame.moves);
+
+          memoryGame.rating(memoryGame.moves);
+
+          // If the selected cards match
+          if ($('.selected').first().find('i').attr('class').substr(6) === $('.selected').last().find('i').attr('class').substr(6)) {
+
+            $('.selected').each(function() {
+              $(this).addClass('matched bounce').removeClass('selected');
+            });
+
+            memoryGame.winGame();
+
+          } else {
+
+            // Remove icons
+            $('.selected').empty();
+          }
+
+          // Deselect cards
+          $('.selected').each(function() {
+            $(this).removeClass('selected');
+          });
+        }
+      }, 1000);
+    },
+
+    // Set player rating (stars)
+    rating: function(moves) {
+
+      var numCards = memoryGame.figures.length;
+
+      var starRating = [numCards + 6, numCards + 4, numCards + 2];
+
+      if (moves > starRating[2] && moves < starRating[1]) {
+        $('.score-panel').find('i').eq(2).removeClass('fa-star').addClass('fa-star-o');
+
+      } else if (moves > starRating[1] && moves < starRating[0]) {
+
+        $('.score-panel').find('i').eq(1).removeClass('fa-star').addClass('fa-star-o');
+
+      } else if (moves > starRating[0]) {
+        $('.score-panel').find('i').eq(0).removeClass('fa-star').addClass('fa-star-o');
+      }
+    },
+
+    // Check if it's a win
+    winGame: function() {
+
+      if ($('.matched').length === $('.card').length) {
+
+        timer.stop();
+
+        // Display win popup
+        $.magnificPopup.open({
+          items: {
+            src: '#win-popup',
+          },
+          type: 'inline'
+        });
+
+        memoryGame.rating(memoryGame.moves);
+
+        memoryGame.generateCards();
+      }
+    }
+  };
+
+  memoryGame.startGame();
+
+  $('.open-popup-link').magnificPopup({
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    type:'inline',
+    midClick: true,
+    overflowY: 'hidden'
+  });
+
+  $('#restart').click(function() {
+    memoryGame.startGame();
+  });
+});
